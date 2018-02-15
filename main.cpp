8fc6e83247861f026349b01d81a00795888cdad6
Nikitossii <nikitasobakabmx@gmail.com>
2018-02-15 18:12:08 +0300

    Cpp commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+class arrayList{
+private:
+arrayList();
+
+template<typename T>
+T size_tget_size() const;
+
+bool is_empty() const;
+
+int& operator[](int i);
+
+const int& operator[](int i) const;
+
+void push_front(int val);
+
+void pop_front();
+
+int& get_front();
+
+const int & get_front() const;
+
+template<typename T>
+void insert(int pos, T);
+
+void erase(int pos);
+
+~arrayList();
+public:
+
+};
 int main(){
+    
     return 0;
 }
